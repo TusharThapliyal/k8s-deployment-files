5f5fae32553bb3ddff6f4bc3b1d0c27c7b4b0e03
tushar thapliyal <tushar.thapliyal@renewbuy.com>
2024-09-19 16:59:03 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@
 sudo docker login -u $DOCKER_USER_NAME -p $DOCKER_PASSWORD # export value for these variables as env
 sudo docker build -t tusharthapliyal/jupyterhub:latest .
 sudo docker push tusharthapliyal/jupyterhub:latest
+
+sudo mkdir -p /data/local
+sudo chown -R $(id -u $USER):$(id -g $USER) /data/local   # Change ownership to the container's UID:GID
+sudo chmod -R 755 /data/local         # Ensure the container user has read/write access
