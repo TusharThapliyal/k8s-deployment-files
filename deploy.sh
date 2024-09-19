c5e7822bf179ef06416102aa4cd411cc0dbe7670
tushar thapliyal <tushar.thapliyal@renewbuy.com>
2024-09-19 15:08:08 +0530

    ~ jupyterhub-deployment.yaml : added volume mount ~ deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-sudo docker build -t jupyterhub:kube .
-sudo docker run -d -it -p 9090:8888 --name jup jupyterhub:kube
+#! /bin/bash
+sudo docker login -u $DOCKER_USER_NAME -p $DOCKER_PASSWORD # export value for these variables as env
+sudo docker build -t tusharthapliyal/jupyterhub:latest .
+sudo docker push tusharthapliyal/jupyterhub:latest
